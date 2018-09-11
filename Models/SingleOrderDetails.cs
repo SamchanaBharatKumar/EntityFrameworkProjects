5b4575259d47a8553bae07c6dbc8652f1ba1778f
SamchanaBharatKumar <43179865+SamchanaBharatKumar@users.noreply.github.com>
2018-09-11 21:55:48 +0530

    Add files via upload

---
+++ Models/SingleOrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderManagement.Models
+{
+   public class SingleOrderDetails
+   {
+
+      public int UserID { get; set; }
+
+      public Dictionary<int, int> ProductQuantityMapping { get; set; }
+
+      public string Address { get; set; }
+   }
+}
